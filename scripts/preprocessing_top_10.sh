2f0442fa7144fcfa53b8105886cd07cd1b0eb3d5
edwardclem <Flashdrive$$1945>
2019-10-25 21:29:23 -0700

    typo

--- scripts/preprocessing_top_10.sh
+++ scripts/preprocessing_top_10.sh
@@ -4,7 +4,7 @@ SIGNS='["na","HAL","iš","MEŠ","ma","1","du","da","AN","AŠ"]'
 
 luigi --module deepscribe.luigi.ml_input AssignDatasetTask --local-scheduler \
       --imgfolder data/ochre/a_pfa \
-      --hdffolder /data/processed/pfa_new \
+      --hdffolder data/processed/pfa_new \
       --target-size 50 \
       --keep-categories $SIGNS  \
       --fractions '[0.7, 0.1, 0.2]' \
